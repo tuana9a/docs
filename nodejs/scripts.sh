4b26bff242f7546f33abcf453c65aecc93f3c33f
Tuan Nguyen Minh <tuana9a@gmail.com>
2021-12-14 13:38:36 +0700

    add nodejs

---
+++ nodejs/scripts.sh
@@ -0,0 +1,17 @@
+# mặc định là local
+npm install PACKAGE
+
+# cái này sẽ cài global ở AppData của user (window)
+npm install PACKAGE -g 
+
+# để instal với package-lock.json
+npm ci 
+
+# xóa một pkg của project
+npm uninstall PACKAGE 
+
+# liệt kê các pkg đã install trong project
+npm ls 
+
+# tìm kiếm pkg online
+npm search PACKAGE 
