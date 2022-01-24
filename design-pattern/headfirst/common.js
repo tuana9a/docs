c1f9e1a6d669070542a8ecfe3ed95af96f1724eb
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-01-24 10:54:24 +0700

    update design pattern refactor remove common.css common.js

--- design-pattern/headfirst/common.js
+++
@@ -1,5 +0,0 @@
-function load(option = { className: "", pathToClass: "" }) {
-    fetch(option.pathToClass)
-        .then((resp) => resp.text())
-        .then((data) => (document.getElementById(option.className).innerHTML = "<pre>" + data + "</pre>"));
-}
