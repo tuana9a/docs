d236ebf77fe0107e47bf75d37c541f648822cc56
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-02-19 21:05:33 +0700

    update cerbot and add nginx

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -1,8 +1,18 @@
-# generate new cert
+# some example obtain
+sudo certbot certonly -d tuana9a.tech
+sudo certbot certonly -d docs.tuana9a.tech
+sudo certbot certonly -d assistant.tuana9a.tech
+sudo certbot certonly -d captcha2text.tuana9a.tech
+sudo certbot certonly -d calendar.tuana9a.tech
+
+# using wildcards with cloudflare
 sudo certbot certonly \
 --dns-cloudflare \
 --dns-cloudflare-credentials ~/.cloudflare.ini \
 -d tuana9a.tech,*.tuana9a.tech
+
+# vị trí của cert
+# VD: /etc/letsencrypt/live/tuana9a.tech/
 
 # remove exist domain
 sudo certbot delete
