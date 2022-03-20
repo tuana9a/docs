3f7cdb2c4a409cb5f06df5d399e8e7cb4003f228
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-03-20 11:16:01 +0700

    update docs app

--- src/main/java/com/tuana9a/docs/DocsApplication.java
+++ src/main/java/com/tuana9a/docs/DocsApplication.java
@@ -2,6 +2,9 @@ package com.tuana9a.docs;
 
 import java.io.IOException;
 
+import com.tuana9a.docs.config.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +12,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DocsApplication implements CommandLineRunner {
 
+    @Autowired
+    private Config config;
+
     public static void main(String[] args) {
         SpringApplication.run(DocsApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws IOException {
+        System.out.println("ROOT_DIR: " + config.ROOT_DIR);
     }
 }
--- start.sh
+++
@@ -1,4 +0,0 @@
-docker run -p 127.0.0.1:4000:4000 \
--v "$(pwd):/app" \
---env-file .env \
-tuana9a/docs:1.0.0
