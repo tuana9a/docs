d30ba1a284b4cb1c32425a6e1de0dfab4e5be28f
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-03-25 06:57:22 +0700

    update: add load animation

--- README.md
+++ README.md
@@ -2,15 +2,18 @@
 
 my personal note about everything I've learned
 
-I've deploy all my docs to [docs.tuana9a.tech](https://docs.tuana9a.tech)
-
 # how to run
 
 ```bash
-# feel free to change bind address or port
+# build first
+# by default after runing below command the output will be in "target/" 
+mvn package
+
+# change bind address or port if you want
 # window or linux with application.properties
-java -jar docs-1.0.0.jar --server.port=80 --server.address=127.0.0.1
+java -jar ./target/docs-1.0.0.jar --server.port=80 --server.address=127.0.0.1
 
 # linux with env variable
-PORT=80 ADDRESS=127.0.0.1 nohup java -jar docs-1.0.0.jar >> run.log 2>&1 & echo $! > run.pid
+PORT=80 ADDRESS=127.0.0.1 java -jar ./target/docs-1.0.0.jar
+PORT=80 ADDRESS=127.0.0.1 nohup java -jar ./target/docs-1.0.0.jar >> run.log 2>&1 & echo $! > run.pid
 ```
--- src/main/webapp/fetchAppend.js
+++ src/main/webapp/fetchAppend.js
@@ -1,33 +1,85 @@
-async function fetchAppendHtml(element, url) {
-    return fetch(url)
-        .then((resp) => resp.text())
-        .then((data) => {
-            const preElement = document.createElement("pre");
-            preElement.innerHTML = data;
-            element.appendChild(preElement);
-        });
-}
-async function fetchAppendRaw(element, url) {
-    return fetch(url)
-        .then((resp) => resp.text())
-        .then((data) => {
-            const preElement = document.createElement("pre");
+class Entry {
+    constructor({ element, baseUrl, url, renderType, onLoad }) {
+        this.baseUrl = baseUrl;
+        this.url = url;
+        this.element = element;
+        this.renderType = renderType;
+        this.onLoad =
+            onLoad ||
+            function () {
+                this.render();
+            };
+        this.data = null;
+    }
+    getFullUrl() {
+        const base = this.baseUrl;
+        const url = this.url;
+        if (!base) {
+            return url;
+        }
+        if (base.endsWith("/")) {
+            base = base.slice(0, -1);
+        }
+        if (url.startsWith("/")) {
+            url = url.substring(1);
+        }
+        return base + "/" + url;
+    }
+    startLoading() {
+        // init loading state
+        let style = "background-color: #000800;";
+        style += "color: #73ff00;";
+        style += "padding: 2px 5px;";
+        style += "font-family: monospace";
+        this.element.innerHTML = `<strong style="${style}">Loading...</strong>`;
+    }
+    stopLoading() {
+        // reset;
+        this.element.innerHTML = "";
+    }
+    async load() {
+        let url = this.getFullUrl();
+        const entry = this;
+        return fetch(url)
+            .then((res) => res.text())
+            .then((data) => {
+                entry.data = data;
+                entry.stopLoading();
+                entry.onLoad(data);
+            });
+    }
+    render() {
+        const preElement = document.createElement("pre");
+        const data = this.data;
+        if (this.renderType == "raw") {
             preElement.innerText = data;
-            element.appendChild(preElement);
-        });
+        } else {
+            preElement.innerHTML = data;
+        }
+        this.element.appendChild(preElement);
+    }
 }
 
 function main() {
-    const fetchAppendElements = Array.from(document.getElementsByClassName("fetchAppend"));
-    for (const element of fetchAppendElements) {
+    const elements = Array.from(document.getElementsByClassName("fetchAppend"));
+    const entries = elements.map(function (element) {
         let url = element.getAttribute("data-url");
+        let baseUrl = null; // FETCH_APPEND_BASE_URL;
+        let opts = {};
+        opts.element = element;
+        opts.url = url;
+        opts.baseUrl = baseUrl;
         if (element.classList.contains("raw")) {
-            fetchAppendRaw(element, url);
-        } else if (element.classList.contains("useRootUrl")) {
-            fetchAppendHtml(element, ROOT_URL + url);
-        } else {
-            fetchAppendHtml(element, url);
+            opts.renderType = "raw";
         }
-    }
+        const entry = new Entry(opts);
+        entry.startLoading();
+        return entry;
+    });
+    setTimeout(async function () {
+        for (const entry of entries) {
+            await entry.load();
+        }
+    }, Math.random() * 100);
 }
 main();
