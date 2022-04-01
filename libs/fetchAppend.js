e7159aed0ae4922f4826103d3f6796e9cde150d1
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-04-01 20:33:47 +0700

    update: support caching make loading more fun

--- libs/fetchAppend.js
+++ libs/fetchAppend.js
@@ -26,16 +26,10 @@ class Entry {
         return base + "/" + url;
     }
     startLoading() {
-        // init loading state
-        let style = "background-color: #000800;";
-        style += "color: #73ff00;";
-        style += "padding: 2px 5px;";
-        style += "font-family: monospace";
-        this.element.innerHTML = `<strong style="${style}">Loading...</strong>`;
+        this.element.classList.add("loading");
     }
     stopLoading() {
-        // reset;
-        this.element.innerHTML = "";
+        this.element.classList.remove("loading");
     }
     async load() {
         let url = this.getFullUrl();
@@ -60,26 +54,59 @@ class Entry {
     }
 }
 
-function main() {
-    const elements = Array.from(document.getElementsByClassName("fetchAppend"));
-    const entries = elements.map(function (element) {
-        let url = element.getAttribute("data-url");
-        let baseUrl = null; // FETCH_APPEND_BASE_URL;
-        let opts = {};
-        opts.element = element;
-        opts.url = url;
-        opts.baseUrl = baseUrl;
-        if (element.classList.contains("raw")) {
-            opts.renderType = "raw";
-        }
-        const entry = new Entry(opts);
-        entry.startLoading();
-        return entry;
-    });
-    setTimeout(async function () {
-        for (const entry of entries) {
-            await entry.load();
-        }
-    }, Math.random() * 100);
+// Create new link Element
+let styleElement = document.createElement("style");
+styleElement.textContent = `
+.loading {
+    --size: 13px;
+    --ring-size: 3px;
+    border: var(--ring-size) solid #f3f3f3;
+    border-radius: 50%;
+    border-top: var(--ring-size) solid #3498db;
+    width: var(--size);
+    height: var(--size);
+    -webkit-animation: spin 2s linear infinite; /* Safari */
+    animation: spin 2s linear infinite;
 }
-main();
+
+/* Safari */
+@-webkit-keyframes spin {
+    0% {
+        -webkit-transform: rotate(0deg);
+    }
+    100% {
+        -webkit-transform: rotate(360deg);
+    }
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+`;
+// Append link element to HTML head
+document.head.appendChild(styleElement);
+// start load entry
+const entries = Array.from(document.getElementsByClassName("fetchAppend")).map(function (element) {
+    let url = element.getAttribute("data-url");
+    let baseUrl = null; // FETCH_APPEND_BASE_URL;
+    let opts = {};
+    opts.element = element;
+    opts.url = url;
+    opts.baseUrl = baseUrl;
+    if (element.classList.contains("raw")) {
+        opts.renderType = "raw";
+    }
+    const entry = new Entry(opts);
+    entry.startLoading();
+    return entry;
+});
+setTimeout(async function () {
+    for (const entry of entries) {
+        await entry.load();
+    }
+}, 10);
--- src/main/node/index.js
+++ src/main/node/index.js
@@ -11,28 +11,41 @@ function createFaviconUrl(token) {
     return "https://avatars.dicebear.com/api/identicon/" + encodeValue + ".svg";
 }
 
+const cache1day = {
+    maxAge: "86400",
+};
+
+const setContentType = function (req, res, next) {
+    next();
+    if (req.path.match(/^.*(\.sh|\.cmd|\.ps1)$/)) {
+        res.set("Content-Type", "text/plain");
+    }
+};
+
+const sendDirectory = function (req, res) {
+    let pathRequest = req.path;
+    let realpath = pathRequest.substring(CONFIG.explorerPrefix.length);
+    let faviconUrl = createFaviconUrl(realpath);
+    let filepaths = fs.readdirSync(path.join(".", realpath));
+    const entries = [];
+    for (let filepath of filepaths) {
+        const isDir = fs.statSync(path.join(".", realpath, filepath)).isDirectory();
+        const entry = {};
+        entry.name = isDir ? filepath + "/" : filepath;
+        entry.url = path.join(CONFIG.explorerPrefix, realpath, filepath);
+        entry.type = isDir ? "d" : "f";
+        entries.push(entry);
+    }
+    res.render("explorer", { faviconUrl: faviconUrl, titleName: pathRequest, entries: entries });
+};
+
 async function main() {
     const server = express();
     server.set("view engine", "ejs");
-    server.use("/", express.static("./src/main/webapp/"));
-    server.use("/libs", express.static("./libs"));
-    server.use(CONFIG.explorerPrefix, express.static("."));
-    server.get(CONFIG.explorerPrefix + "*", function (req, res) {
-        let pathRequest = req.path;
-        let realpath = pathRequest.substring(CONFIG.explorerPrefix.length);
-        let faviconUrl = createFaviconUrl(realpath);
-        let filepaths = fs.readdirSync(path.join(".", realpath));
-        const entries = [];
-        for (let filepath of filepaths) {
-            const isDir = fs.statSync(path.join(".", realpath, filepath)).isDirectory();
-            const entry = {};
-            entry.name = isDir ? filepath + "/" : filepath;
-            entry.url = path.join(CONFIG.explorerPrefix, realpath, filepath);
-            entry.type = isDir ? "d" : "f";
-            entries.push(entry);
-        }
-        res.render("explorer", { faviconUrl: faviconUrl, titleName: pathRequest, entries: entries });
-    });
+    server.use("/", express.static("./src/main/webapp/", cache1day));
+    server.use("/libs", express.static("./libs", cache1day));
+    server.use(CONFIG.explorerPrefix, setContentType, express.static(".", cache1day));
+    server.get(CONFIG.explorerPrefix + "*", sendDirectory);
     let port = CONFIG.port;
     if (CONFIG.ssl.enabled) {
         // https
